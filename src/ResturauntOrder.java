31e25b13d3a8babfe0c300ad86b732396720f97a
15104 <556102@my.cuhsd.org>
2024-12-14 16:36:14 -0800

    added more text

---
+++ src/ResturauntOrder.java
@@ -0,0 +1,12 @@
+/**
+ * @author class that'll validate user's number, save # of meals and loop
+ * user input and return food based on # of meals inputted
+ */
+public class ResturauntOrder {
+    int count;
+    public ResturauntOrder()
+    {
+
+    }
+
+}
--- src/foodGeneratorMain.java
+++ src/foodGeneratorMain.java
@@ -1,5 +1,22 @@
-public class Main {
+/**
+ * main class that'll ask for # of meals they want for the day
+ * then asks to pick a # between 1-12
+ * Takes user input and print out user's food
+ * repeats based on the number of meals they input
+ */
+import java.util.Scanner;
+public class foodGeneratorMain {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //colors codes for each string
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to the " + "\u001B[35m" + "Precious " + "\u001B[33m" + "Lily " + "\u001B[32m" + "Food Generator");
+        System.out.println("How many meals would you like to have today?");
+
+
+
+
     }
 }
