87cdaf9e804d79f99223c808a5b5306dc324ce02
15104 <556102@my.cuhsd.org>
2024-12-19 19:21:53 -0800

    added more text

--- src/ResturauntOrder.java
+++ src/ResturauntOrder.java
@@ -1,165 +1,134 @@
 import java.util.Scanner;
-import java.util.InputMismatchException;
+import java.util.Random;
 
 /**
- * @author class that'll validate user's number, save # of meals and loop
+ * class that'll validate user's number, save # of meals and loop
  * user input and return food based on # of meals inputted
+ * @author Lily and Precious
+ * @version 2.0.0
  */
 public class ResturauntOrder {
     private int numMeals;
-    public int mealOrder;
     private String mealName;
-    private boolean acceptMeal;
+    private String satisfaction;
 
-    //setting a value to numMeals variable and setting acceptMeal to false
+    /**
+     * constructor
+     * @param numMeals number of meals represented in integers
+     */
     public ResturauntOrder(int numMeals)
     {
         this.numMeals = numMeals;
-        this.acceptMeal = false;
     }
 
-    Scanner input = new Scanner(System.in);
-
-
-    //Confirming if number of meals is valid
-    public void validateNumMeals(int numMeals)
+    /**
+     * constructor
+     * @param satisfaction user satisfaction represented in string
+     */
+    public ResturauntOrder(String satisfaction)
     {
-        while (!acceptMeal)
-        {
-            if (numMeals <= 0)
-            {
-                System.out.println("Don't you think you should eat?");
-                System.out.println("How many meals would you like to eat for today?");
-                numMeals = input.nextInt();
-            }
-            else if (numMeals > 3)
-            {
-                System.out.println("A bit much don't you think?");
-                System.out.println("How many meals would you like to eat for today?");
-                numMeals = input.nextInt();
-            }
-            else if (numMeals >= 1 && numMeals <= 3)
-            {
-                System.out.println("Great!");
-                acceptMeal = true;
-                break;
-            }
-        }
-        askForMeal();
+        this.satisfaction = satisfaction;
     }
 
-    //method will ask user to pick between 1-12
-    public String askForMeal()
+
+    /**
+     * generates random meal (within 1-12) by calling on getMeal method
+     * @return random meals for user
+     */
+    public void askForMeal()
     {
-        //StringBuilder mealChoices = new StringBuilder();
-        //looping for however many number of meals the user inputs
+        Random rand = new Random();
+
         for (int i = 0; i < numMeals; i++)
         {
-            System.out.println("Enter a number between 1-12 to get food");
-            mealOrder = input.nextInt();
-
-            if (mealOrder == 1)
-            {
-                System.out.println("A tiny bowl of cereal and room temperature milk");
-            }
-            else if (mealOrder == 2)
-            {
-                //String mealOrder2 = "A nice juicy 'Premium' Wagyu Beef steak with a side of refined water ;)";
-                //return mealOrder2;
-                System.out.println ("A nice juicy 'Premium' Wagyu Beef steak with a side of refined water ;)");
-
-            }
-            else if (mealOrder == 3)
-            {
-                System.out.println("a");
-            }
-            else if (mealOrder == 4)
-            {
-                System.out.println("b");
-            }
-            else if (mealOrder == 5)
-            {
-                System.out.println("c");
-            }
-            else if (mealOrder == 6)
-            {
-                System.out.println("d");
-            }
-            else if (mealOrder == 7)
-            {
-                System.out.println("e");
-            }
-            else if (mealOrder == 8)
-            {
-                System.out.println("f");
-            }
-            else if (mealOrder == 9)
-            {
-                System.out.println("g");
-            }
-            else if (mealOrder == 10)
-            {
-                System.out.println("h");
-            }
-            else if (mealOrder == 11)
-            {
-                System.out.println("i");
-            }
-            else if (mealOrder == 12)
-            {
-                System.out.println("j");
-            }
-            else {
-                System.out.println ("invalid number!");
-            }
+            int mealOrder = rand.nextInt(12) + 1;
+            String meal = getMeal(mealOrder);
+            System.out.println("Meal: " + (i + 1) + " " + meal);
         }
-        return "Thanks!";
+    }
+
+    /**
+     * sorts list of meals using switch statement
+     * @param mealOrder randomly picks amount of meals based on users number
+     * @return list of meals based on mealOrder input
+     */
+    private String getMeal (int mealOrder)
+    {
+        //switch statement to return a random meal otherwise invalid
+        switch (mealOrder)
+        {
+            case 1: return "A hot plate of Lumpia";
+            case 2: return "A sizzling plate of Sising";
+            case 3: return "A warm bowl of fried noodles";
+            case 4: return "A chilly bowl of dirty ice cream!";
+            case 5: return "A cup of crunchy Lechon Kawali";
+            case 6: return "A savory dish of Halo Halo!!";
+            case 7: return "A steaming plate of com tam ̋(ˊ•͈ ꇴ •͈ˋ)و";
+            case 8: return "A rich bowl of Bun Bo Hue! ✧٩(ˊωˋ*)و✧";
+            case 9: return "A delectable bowl of warm Chao ✧٩(ˊωˋ*)و✧";
+            case 10: return "A crispy banh mi sandwich ( ´ ▽ ` )";
+            case 11: return "A refreshing bowl of cold Bun Ga Nuong! (و ˃̵ᴗ˂̵)و";
+            case 12: return "A delicate dish of Banh Da Lon !(•̀ᴗ•́)";
+            default: return "Invalid meal selection.";
+        }
+    }
+
+    /**
+     * grabs random response based on users input using serverResponses method
+     * @param satisfaction the satisfactory rate of the user's meal
+     * @return a servers response
+     */
+    public String serverSatisfaction(String satisfaction)
+    {
+        String response = "";
+        if (satisfaction.indexOf("no") >= 0  || satisfaction.indexOf("nah") >= 0 || satisfaction.indexOf("naur") >= 0 || (satisfaction.indexOf("NO") >= 0))
+        {
+            response = serverResponses();
+        }
+        else
+        {
+            response = "yay";
+        }
+        return response;
     }
 
 
-//public String serverSatisfaction(String satisfaction)
-//{
-    //String response = "";
-    //if (satisfaction.indexOf("no") >= 0)
-    //{
-      //  response = serverResponses();
-    //}
-  //  else if (satisfaction.indexOf("NO") >= 0
-//|| satisfaction.indexOf("nah") >= 0 || satisfaction.indexOf("naur") >= 0)
-//{
-  //  response = serverResponses();
-//}
-  //  else
-//{
-  //  response = "yay";
-//}
-//}
+    /**
+     * generates random response based on a predetermined list of responses
+     * @return the server's response based on users input
+     */
+    public String serverResponses()
+    {
+        int NUMBER_OF_RESPONSES = 4;
+        double responseIndex = Math.random();
+        int typeOfResponse = (int) (responseIndex * NUMBER_OF_RESPONSES);
+        String response = "";
 
-//public String serverResponses()
-//{
-  //  int NUMBER_OF_RESPONSES = 4;
-   // double responseIndex = Math.random();
-   // int typeOfResponse = (int) (responseIndex * NUMBER_OF_RESPONSES);
-   // String response = "";
+        switch (typeOfResponse)
+        {
+            case 0:
 
-    //if (typeOfResponse == 0)
-    //{
-    //    response = "no is not the correct answer >:[";
-    //}
-    //else if (typeOfResponse == 1)
-    //{
-    //    response = "I won't take no for an answer!";
-   // }
-    //else if (typeOfResponse == 2)
-    //{
-   //     response = "You better say yes.";
-  //  }
-   // else if (typeOfResponse == 3)
-   // {
-    //    response = "Please say yes I NEEEEEED it :(";
-  //  }
-  //  return response;
+                response = "\u001B[31m" + "no is not the correct answer >:[";
+                break;
 
-//}
+            case 1:
+                response = "\u001B[31m" + "I won't take no for an answer!";
+                break;
+
+            case 2:
+                response = "\u001B[31m" + "You better say yes.";
+                break;
+
+            case 3:
+                response = "\u001B[36m" + "Please say yes I NEEEEEED it :(";
+                break;
+
+            default:
+                response = "grrrrrrr";
+                break;
+
+        }
+        return response;
+    }
 
 }
--- src/foodGeneratorMain.java
+++ src/foodGeneratorMain.java
@@ -3,35 +3,62 @@
  * then asks to pick a # between 1-12
  * Takes user input and print out user's food
  * repeats based on the number of meals they input
+ * @author Lily and Precious
+ * @version 2.0.0
  */
 import java.util.Scanner;
 public class foodGeneratorMain {
     public static void main(String[] args) {
 
-
         //creating scanner object
         Scanner input = new Scanner(System.in);
 
         //welcome sign for user
-        System.out.println("Welcome to the " + "\u001B[35m" + "Precious " + "\u001B[33m" + "Lily " + "\u001B[32m" + "Food Generator");
+        System.out.println("Welcome to the " + "\u001B[35m" + "Precious " + "\u001B[33m" + "Lily " + "\u001B[32m" + "Food Generator" + "\u001B[0m");
+
         //asking for user input on number of meals they want
         System.out.println("How many meals would you like to have today?");
         int userMeals = input.nextInt();
+
+        //checking to make sure meals are between 1-12 since there are 12 options
+        while (userMeals < 1 || userMeals > 12)
+        {
+            System.out.println ("Invalid number of meals. Please enter a number between 1 and 12.");
+            userMeals = input.nextInt();
+        }
         ResturauntOrder userTotalMeal = new ResturauntOrder(userMeals);
-        userTotalMeal.validateNumMeals(userMeals);
+        userTotalMeal.askForMeal();
+
+
+        input.nextLine();
+        String userSatis = "";
+
+        //while loop--confirming if user is satisfied with their meals
+        while (true)
+        {
+
+            System.out.println("Were you satisfied with your meal?");
+            userSatis = input.nextLine();
+
+            if (userSatis.indexOf("no") >= 0 || userSatis.indexOf("NO") >= 0)
+            {
+                ResturauntOrder userSatisfaction = new ResturauntOrder(userSatis);
+                String response1 = userSatisfaction.serverSatisfaction(userSatis);
+                System.out.println(response1);
+            }
+            else if (userSatis.indexOf("yes") >= 0 || userSatis.indexOf("YES") >= 0 )
+            {
+                System.out.println ("Yay :3");
+                break;
+            }
+            else
+            {
+                ResturauntOrder userSatisfaction = new ResturauntOrder(userSatis);
+                String response1 = userSatisfaction.serverSatisfaction(userSatis);
+                System.out.println(response1);
+            }
+        }
+
         input.close();
-
-        //asking user to pick a number between 1-12
-        System.out.println("Ok! Please pick a number between 1-12");
-        int userNum = input.nextInt();
-        ResturauntOrder userNumMeal = new ResturauntOrder (userNum);
-        userNumMeal.askForMeal();
-        //input.close();
-
-       // ResturauntOrder userSatisfaction = new ResturauntOrder();
-       // System.out.println("Were you satisfied with your meal?");
-       // String userSatis = input.nextLine();
-       // userSatisfaction.serverSatisfaction(userSatis);
-
     }
 }
